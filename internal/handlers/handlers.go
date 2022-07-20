5adcc7e63e88a2bd36d9ce98fce8eb3313f77972
Krynegal <andrey.senyakin@gmail.com>
2022-07-20 21:15:10 +0300

    increment 4

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/Krynegal/url_shortener.git/internal/storage"
 	"github.com/gorilla/mux"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -13,14 +17,50 @@ type Handler struct {
 	Storage storage.IStorage
 }
 
+type RequestAPI struct {
+	URL string `json:"url"`
+}
+
+type ResponseAPI struct {
+	Result string `json:"result"`
+}
+
 func NewHandler(storage storage.IStorage) *Handler {
 	h := &Handler{
 		Mux:     mux.NewRouter(),
 		Storage: storage,
 	}
 	h.Mux.Handle("/", h.ShortURL()).Methods(http.MethodPost)
+	h.Mux.Handle("/api/shorten", h.Shorten()).Methods(http.MethodPost)
 	h.Mux.Handle("/{id}", h.GetID()).Methods(http.MethodGet)
 	return h
+}
+
+func (h *Handler) Shorten() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		var req RequestAPI
+		buf := bytes.NewBuffer(body)
+		if err = json.NewDecoder(buf).Decode(&req); err != nil {
+			http.Error(w, "wrong request format", http.StatusBadRequest)
+			return
+		}
+		log.Printf("req: %v", req)
+		newID := h.Storage.Shorten(req.URL)
+
+		resp := ResponseAPI{Result: fmt.Sprintf("http://localhost:8080/%s", strconv.Itoa(newID))}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		if err = json.NewEncoder(w).Encode(resp); err != nil {
+			http.Error(w, "troubles with encode response", http.StatusBadRequest)
+			return
+		}
+	}
 }
 
 func (h *Handler) ShortURL() http.HandlerFunc {
